7650b801e86bae4edeb5ad95fb3f28a2c2fbc917
Piotr Kubiak <kuberxxx1986@gmail.com>
2022-01-19 08:35:20 +0100

    normalize, globastyle and theme to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-function App() {
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { themeLight, themeDark } from "./theme";
+import { Normalize } from "styled-normalize";
+import { selectIsDarkTheme } from "./common/themeSlice";
+import { PersonalHomepage } from "./features/PersonalHomepage";
+
+export const App = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
   return (
-    <div>
-      <header>Strona w budowie!!!</header>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+      <Normalize />
+      <GlobalStyle />
+      <PersonalHomepage />
+    </ThemeProvider>
   );
 }
 
