afee699e7e68c41e95f008fb44cc791a29d2ab23
Piotr Kubiak <kuberxxx1986@gmail.com>
2022-01-21 13:54:59 +0100

    add div to Wrapper

--- src/features/PersonalHomepage/MainInformation/index.js
+++ src/features/PersonalHomepage/MainInformation/index.js
@@ -5,16 +5,16 @@ import { Avatar, StyledButtonLink, ButtonIcon, Name, Summary, ThisIs, Wrapper } 
 export const MainInformation = () => (
   <Wrapper>
     <Avatar src={profileImage} alt="Piotr Kubiak" />
-    <>
+    <div>
       <ThisIs>This is</ThisIs>
       <Name>Piotr Kubiak</Name>
       <Summary>
-        🧑💻I'm passionate about Frontend mainly JavaScript React, and currently looking for new job.
+        🧑💻 I'm passionate about Frontend mainly JavaScript React, and currently looking for new job.
       </Summary>
       <StyledButtonLink href={`mailto:${email}`} title={email}>
         <ButtonIcon />
         Hire me
       </StyledButtonLink>
-    </>
+    </div>
   </Wrapper>
 );
