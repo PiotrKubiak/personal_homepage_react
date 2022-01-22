ca3aac300f771e700d5c516aa07bcc0c78cb35f5
Piotr Kubiak <kuberxxx1986@gmail.com>
2022-01-22 12:09:50 +0100

    add NewSkills function

--- src/features/PersonalHomepage/Skills/index.js
+++ src/features/PersonalHomepage/Skills/index.js
@@ -1,5 +1,5 @@
 import { Section, StyledHeader, List, Item, Bullet } from "./styled";
-import { skills } from "../skillsData";
+import { skills, nextSkills } from "../skillsData";
 
 export const Skills = ({ title }) => (
   <Section>
@@ -14,3 +14,18 @@ export const Skills = ({ title }) => (
     </List>
   </Section>
 );
+
+export const NextSkills = ({ title }) => (
+  <Section>
+    <StyledHeader>{title}</StyledHeader>
+    <List>
+      {nextSkills.map(skill => (
+        <Item key={skill}>
+          <Bullet />
+          {skill}
+        </Item>
+      ))}
+    </List>
+  </Section>
+);
+
