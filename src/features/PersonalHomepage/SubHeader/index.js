d525db28a5b0cb06b7d92e0e95b29d18c65a38ea
Piotr Kubiak <kuberxxx1986@gmail.com>
2022-01-23 16:24:04 +0100

    correct text color in subHeader

--- src/features/PersonalHomepage/SubHeader/index.js
+++ src/features/PersonalHomepage/SubHeader/index.js
@@ -4,7 +4,7 @@ export const SubHeader = styled.h2`
   margin: 0;
   font-size: 30px;
   font-weight: 900;
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.textPrimary};
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
     font-size: 18px;
